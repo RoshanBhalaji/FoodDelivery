329931a921f7d9aaf25919cf6ef3847613fdfc10
Roshan <120234833+RoshanBhalaji@users.noreply.github.com>
2024-06-17 16:04:20 +0530

    Update vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'build', // Specify the output directory for production builds
+    outDir: 'build', // Specify your build output directory
+    assetsDir: 'assets', // Optional: Specify your assets directory
   },
 });
